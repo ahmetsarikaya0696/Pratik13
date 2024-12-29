df875c6e2226baa12926710fe991c9e33b66dc58
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2024-12-30 02:34:45 +0300

    Add project files.

---
+++ Pratik13/Kitap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik13
+{
+    public class Kitap
+    {
+        public Kitap()
+        {
+            KayitTarihi = DateOnly.FromDateTime(DateTime.Now);
+        }
+
+        public Kitap(string ad, string yazarAdi, int sayfaSayisi, string yayinEvi)
+        {
+            Ad = ad;
+            YazarAdi = yazarAdi;
+            SayfaSayisi = sayfaSayisi;
+            Yayinevi = yayinEvi;
+            KayitTarihi = DateOnly.FromDateTime(DateTime.Now);
+        }
+
+        public string Ad { get; set; }
+        public string YazarAdi { get; set; }
+        public string YazarSoyadi { get; set; }
+        public int SayfaSayisi { get; set; }
+        public string Yayinevi { get; set; }
+        public DateOnly KayitTarihi { get; set; }
+    }
+}
---
+++ Pratik13/Program.cs
@@ -0,0 +1,14 @@
+﻿using Pratik13;
+
+// Class : Bu örneğimizde 'Kitap' bir class'tır. (Bina planı gibi düşünebiliriz)
+// Property : Kitap class'ında Ad, YazarAdi, YazarSoyadi, SayfaSayisi, Yayinevi ve KayitTarihi property'leridir. Fieldları encapsulate ederler.
+// New : Kitap class'ından bir nesne oluşturmak için 'new' anahtar kelimesi kullanılır. (Bina inşa etmek gibi düşünebiliriz)
+Kitap kitap1 = new Kitap()
+{
+    Ad = "Adı Aylin",
+    YazarAdi = "Ayşe",
+    YazarSoyadi = "Kulin",
+    SayfaSayisi = 398,
+    Yayinevi = "Remzi Kitabevi"
+};
+Kitap kitap2 = new Kitap("Adı Aylin", "Ayşe", 398, "Remzi Kitabevi");
